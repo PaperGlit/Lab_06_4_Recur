19cd80ee0e621b5baa8a3ae441b8f1d6c3336a04
Glit <vpad.lazar.04@gmail.com>
2022-11-13 20:58:25 +0200

    Add project files.

---
+++ Lab_06_4_Recur/Lab_06_4_Recur.cpp
@@ -0,0 +1,135 @@
+﻿#include <iostream>
+#include <time.h>
+#include <windows.h>
+
+using namespace std;
+
+void Create(int* a, const int size, const int Low, const int High, int i)
+{
+	if (i < size)
+	{
+		a[i] = Low + rand() % (High - Low + 1);
+		Create(a, size, Low, High, i + 1);
+	}
+}
+
+void Print(int* a, const int size, int i)
+{
+	if (i < 1)
+		cout << "Значення масиву : ";
+	if (i < size)
+	{
+		cout << a[i] << " ";
+		Print(a, size, i + 1);
+	}
+	else
+		cout << endl;
+}
+
+int Min(int* a, const int size, int i, int min, int imin)
+{
+	if (i < size)
+	{
+		if (abs(a[i]) < min)
+		{
+			min = abs(a[i]);
+			imin = i;
+		}
+		Min(a, size, i + 1, min, imin);
+	}
+	else
+		return imin;
+}
+
+int Sum1(int* a, const int size, int j, int n)
+{
+	if (j < size)
+	{
+		n += abs(a[j]);
+		Sum1(a, size, j + 1, n);
+	}
+	else 
+		return n;
+}
+
+int Sum(int* a, const int size, int i, int s)
+{
+	if (i < size)
+	{
+		if (a[i] < 0)
+		{
+			s = Sum1(a, size, i + 1, 0);
+			return s;
+		}
+		Sum(a, size, i + 1, s);
+	}
+	else
+		return 0;
+}
+
+int* Sort(int* a, const int size, int j)
+{
+	if (j < size - 1)
+	{
+		a[j] = a[j + 1];
+		a[j + 1] = 0;
+		Sort(a, size, j + 1);
+	}
+	else
+		return a;
+}
+
+const int N(int* a, const int size, int i, int k, int b, int c)
+{
+	if (i < size)
+	{
+		if (a[i] >= b && a[i] <= c)
+			k++;
+		N(a, size, i + 1, k, b, c);
+	}
+	else
+	{
+		const int n = size - k;
+		return n;
+	}
+}
+
+int* Compress(int* a, const int size, int i, int b, int c)
+{
+	if (i < size)
+	{
+		if (a[i] >= b && a[i] <= c)
+		{
+			Sort(a, size, i);
+			Compress(a, size, i, b, c);
+		}
+		else
+			Compress(a, size, i + 1, b, c);
+	}
+	else
+		return a;
+}
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	srand((unsigned)time(NULL));
+
+	int n, Low, High, a, b;
+
+	cout << "n = "; cin >> n;
+	cout << "min = "; cin >> Low;
+	cout << "max = "; cin >> High;
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+
+	int* c = new int[n];
+
+	Create(c, n, Low, High, 0);
+	Print(c, n, 0);
+	cout << "imin = " << Min(c, n, 1, abs(c[0]), 0) << endl;
+	cout << "Sum = " << Sum(c, n, 0, 0) << endl;
+	Print(Compress(c, n, 0, a, b), N(c, n, 0, 0, a, b), 0);
+
+	return 0;
+}
---
+++ UnitTest_Lab_06_4_Recur/UnitTest_Lab_06_4_Recur.cpp
@@ -0,0 +1,110 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_06_4_Recur/Lab_06_4_Recur.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+void Loop(int* a, int* c, const int size, int i)
+{
+	if (i < size)
+	{
+		Assert::AreEqual(a[i], c[i]);
+		Loop(a, c, size, i + 1);
+	}
+}
+
+int TestMin(int* a, const int size, int i, int min, int imin)
+{
+	if (i < size)
+	{
+		if (abs(a[i]) < min)
+		{
+			min = abs(a[i]);
+			imin = i;
+		}
+		TestMin(a, size, i + 1, min, imin);
+	}
+	else
+		return imin;
+}
+
+int TestSum1(int* a, const int size, int i, int n)
+{
+	if (i < size)
+	{
+		n += abs(a[i]);
+		TestSum1(a, size, i + 1, n);
+	}
+	else
+		return n;
+}
+
+int TestSum(int* a, const int size, int i, int n)
+{
+	if (i < size)
+	{
+		if (a[i] < 0)
+		{
+			n = TestSum1(a, size, i + 1, 0);
+			return n;
+		}
+		TestSum(a, size, i + 1, n);
+	}
+	else
+		return 0;
+}
+
+int* TestSort(int* a, const int size, int j)
+{
+	if (j < size - 1)
+	{
+		a[j] = a[j + 1];
+		a[j + 1] = 0;
+		Sort(a, size, j + 1);
+	}
+	else
+		return a;
+}
+
+int* TestCompress(int* a, const int size, int i, int b, int c)
+{
+	if (i < size)
+	{
+		if (a[i] >= b && a[i] <= c)
+		{
+			Sort(a, size, i);
+			Compress(a, size, i, b, c);
+		}
+		else
+			Compress(a, size, i + 1, b, c);
+	}
+	else
+		return a;
+}
+
+void Test(int* a, int* n, const int size, int b, int c)
+{
+	Assert::AreEqual(TestMin(a, size, 1, abs(a[0]), 0), Min(a, size, 1, abs(a[0]), 0));
+	Assert::AreEqual(TestSum(a, size, 0, 0), Sum(a, size, 0, 0));
+	a = (TestCompress(a, size, 0, b, c));
+	n = (Compress(n, size, 0, b, c));
+	Loop(a, n, size, 0);
+}
+
+namespace UnitTestLab064Recur
+{
+	TEST_CLASS(UnitTestLab064Recur)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			const int n = 10;
+			int c[n];
+			int Low = -10, High = 10, a = 5, b = 10;
+			int* d = c;
+			Create(c, n, Low, High, 0);
+			Test(c, d, n, a, b);
+		}
+	};
+}
